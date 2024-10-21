f87b63bb88180b890a38a9055c89591d636d6dec
minju <kminju0224@gmail.com>
2024-10-21 14:15:56 +0900

    feat: css 기본 설정

--- itta-bab-frontend/src/components/common/Header.vue
+++ itta-bab-frontend/src/components/common/Header.vue
@@ -1,11 +1,56 @@
 <script setup>
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
-
 <template>
-
+  <header>
+    <div class="headerWrap">
+      <div id="logo">itta-bab</div>
+      <ul class="header-list">
+        <li>맛집 리스트</li>
+        <li>모임 참여</li>
+        <li>익명 커뮤니티</li>
+      </ul>
+      <div class="user-tap">
+        <div id="alarm"><font-awesome-icon :icon="['fas', 'bell']" /></div>
+        <div id="mypage"><font-awesome-icon :icon="['far', 'user']" /></div>
+        <div id="log-out"><font-awesome-icon :icon="['fas', 'arrow-right-from-bracket']" /></div>
+      </div>
+    </div>
+  </header>
 </template>
-
 <style scoped>
-
+@import '../../resetcss.css';
+*{
+  color: var(--text-color);
+}
+.headerWrap{
+  display: flex;
+  height: 100px;
+  align-items: center;
+  justify-content: space-between;
+}
+#logo{
+  font-size: 35px;
+  margin-left: 50px;
+  font-weight: 600;
+}
+.header-list{
+  font-size: 20px;
+}
+li {
+  display: inline;
+  margin:25px;
+}
+.user-tap{
+  display: flex;
+  height: 100px;
+  align-items: center; /* 세로 가운데 정렬 */
+}
+.user-tap div{
+  font-size: 25px;
+  margin: 10px;
+}
+#log-out{
+  margin-right: 50px;
+}
 </style>
--- itta-bab-frontend/src/main.js
+++ itta-bab-frontend/src/main.js
@@ -1,4 +1,18 @@
+// main.js
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBell } from '@fortawesome/free-solid-svg-icons' // solid로 대체
+import { faUser as faRegularUser } from '@fortawesome/free-regular-svg-icons'
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).mount('#app')
+// 아이콘들을 라이브러리에 추가
+library.add(faBell, faRegularUser, faArrowRightFromBracket)
+
+const app = createApp(App)
+
+// FontAwesomeIcon 컴포넌트를 전역으로 등록
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')

c2468175cfb51a4baabce527d584eef4501b08ce
Pangtaek <zombie3632@gmail.com>
2024-10-21 19:03:39 +0900

    feat: 모임 상세 정보 UI

--- itta-bab-frontend/src/main.js
+++ itta-bab-frontend/src/main.js
@@ -1,18 +1,26 @@
-// main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBell } from '@fortawesome/free-solid-svg-icons' // solid로 대체
-import { faUser as faRegularUser } from '@fortawesome/free-regular-svg-icons'
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import {createApp} from 'vue';
+import App from './App.vue';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import {faArrowRightFromBracket, faBell, faFlag} from '@fortawesome/free-solid-svg-icons'; // solid로 대체
+import {faUser} from '@fortawesome/free-regular-svg-icons'
+import {faInstagram, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {faClock} from "@fortawesome/free-regular-svg-icons/faClock";
+import {faReceipt} from "@fortawesome/free-solid-svg-icons/faReceipt";
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 // 아이콘들을 라이브러리에 추가
-library.add(faBell, faRegularUser, faArrowRightFromBracket)
+library.add(faBell, faUser, faArrowRightFromBracket, faUserPlus);
 
-const app = createApp(App)
+// SNS 아이콘
+library.add(faInstagram, faYoutube);
+
+// 임광택
+library.add(faClock, faReceipt, faFlag);
+
+const app = createApp(App);
 
 // FontAwesomeIcon 컴포넌트를 전역으로 등록
-app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('font-awesome-icon', FontAwesomeIcon);
 
-app.mount('#app')
+app.mount('#app');

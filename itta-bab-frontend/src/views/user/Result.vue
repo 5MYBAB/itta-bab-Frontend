9e169d2fe8bc3bce74cb73be73aacb04171da35e
seolbin01 <se01bin@suwon.ac.kr>
2024-10-22 13:46:12 +0900

    feat: 결과창 구현

---
+++ itta-bab-frontend/src/views/user/Result.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ResultDetail from "@/components/user/ResultDetail.vue";
+</script>
+
+<template>
+  <div class="back">
+    <ResultDetail/>
+    </div>
+</template>
+
+<style scoped>
+.back {
+  padding-top: 30px;
+  background-color: var(--background-color);
+  display: flex;
+  justify-content: center;
+  align-items: center; /* 세로 가운데 정렬 */
+  flex-direction: column;
+}
+</style>

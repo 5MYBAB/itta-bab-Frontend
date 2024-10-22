d6cda7cbf79286aa5d561eb458aacfd2485d619f
seolbin01 <se01bin@suwon.ac.kr>
2024-10-22 17:08:28 +0900

    feat: FullCalendar 설치

--- itta-bab-frontend/src/views/Home.vue
+++ itta-bab-frontend/src/views/Home.vue
@@ -1,27 +1,57 @@
-<script setup>
-import {useRoute, useRouter} from "vue-router";
+<!--<script setup>-->
+<!--import {useRoute, useRouter} from "vue-router";-->
 
-const currentRoute = useRoute();
+<!--const currentRoute = useRoute();-->
 
-const router = useRouter();
+<!--const router = useRouter();-->
 
-function goToLogin() {
-  router.push('/user/login')
-}
+<!--function goToLogin() {-->
+<!--  router.push('/user/login')-->
+<!--}-->
 
-function goToSignUp() {
-  router.push('/user/signup')
+<!--function goToSignUp() {-->
+<!--  router.push('/user/signup')-->
+<!--}-->
+
+<!--</script>-->
+
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data: function() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        weekends: false,
+        events: [
+          { title: 'Meeting', start: new Date() }
+        ]
+      }
+    }
+  }
 }
 </script>
 
+<!--<template>-->
+<!--  <div>-->
+<!--    <h1>Home 화면 입니다.</h1>-->
+<!--    <button @click="goToLogin">로그인</button>-->
+<!--    <button @click="goToSignUp">회원가입</button>-->
+<!--  </div>-->
+<!--</template>-->
+
 <template>
-  <div>
-    <h1>Home 화면 입니다.</h1>
-    <button @click="goToLogin">로그인</button>
-    <button @click="goToSignUp">회원가입</button>
-  </div>
+  <h1>Demo App</h1>
+  <FullCalendar :options='calendarOptions' />
 </template>
 
+<!--
 <style scoped>
 div{
   display: flex;
@@ -36,4 +66,4 @@ h1{
 button {
   font-size: 40px;
 }
-</style>
+</style>-->

53510402e93851cbf58318008cbf28a4cde9e0b2
minju <kminju0224@gmail.com>
2024-10-22 17:59:51 +0900

    feat: mypage userinfo 구현

--- itta-bab-frontend/src/main.js
+++ itta-bab-frontend/src/main.js
@@ -8,9 +8,12 @@ import {faInstagram, faYoutube} from '@fortawesome/free-brands-svg-icons';
 import {faClock} from "@fortawesome/free-regular-svg-icons/faClock";
 import {faReceipt} from "@fortawesome/free-solid-svg-icons/faReceipt";
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
-
+import {faSmile} from "@fortawesome/free-regular-svg-icons/faSmile";
+import {faMap} from "@fortawesome/free-regular-svg-icons/faMap";
+import {faMobileScreen} from "@fortawesome/free-solid-svg-icons/faMobileScreen";
+import {faUserGroup} from "@fortawesome/free-solid-svg-icons/faUserGroup";
 // 아이콘들을 라이브러리에 추가
-library.add(faBell, faUser, faArrowRightFromBracket, faUserPlus);
+library.add(faBell, faUser, faArrowRightFromBracket, faUserPlus, faSmile ,faMap, faMobileScreen,faUserGroup);
 
 // SNS 아이콘
 library.add(faInstagram, faYoutube);

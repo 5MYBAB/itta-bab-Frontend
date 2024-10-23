4879a2a7961e7f0a8c146a37f8b4567be49cf8f5
minju <kminju0224@gmail.com>
2024-10-23 11:26:07 +0900

    feat: mypage 친구 목록 구현

--- itta-bab-frontend/src/components/user/FriendsList.vue
+++ itta-bab-frontend/src/components/user/FriendsList.vue
@@ -1,11 +1,72 @@
 <script setup>
-
+import '@/assets/css/resetcss.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 </script>
 
 <template>
+  <div class="display">
+    <div class="title">
+      <font-awesome-icon :icon="['fas', 'user-group']" size="2x" />
+      <div>친구 목록</div>
+    </div>
+  </div>
+    <div class="list-container">
+      <div class="list-content">
+        <div class="contents">
+          <div class="text">1</div>
+          <div class="text">가나다</div>
+          <div class="delete">삭제</div>
+        </div>
+      </div>
+      <div class="list-content">
+        <div class="contents">
+          <div class="text">1</div>
+          <div class="text">가나다</div>
+          <div class="delete">삭제</div>
+        </div>
+      </div>
+    </div>
 
 </template>
 
 <style scoped>
-
+.delete{
+  color: var(--gray-font);
+  font-size: 15px;
+}
+.contents{
+  display: flex;
+  justify-content: center;
+  gap: 230px;
+  margin: 20px 0px;
+}
+.text{
+  font-size: 20px;
+  font-weight: 400;
+}
+.list-container{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.list-content{
+  width: 80%;
+  border-bottom: 2px var(--gray-font) solid;
+}
+.display{
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+}
+.title{
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+}
+.title div{
+  font-size: 30px;
+  font-weight: 600;
+  margin-left: 10px;
+}
 </style>

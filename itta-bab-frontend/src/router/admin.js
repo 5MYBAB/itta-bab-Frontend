00742ad52793142bc0591dd0b645942f7a95b105
sung-mini <sungmini9755@naver.com>
2024-10-24 11:42:59 +0900

    feat: 관리자 부트캠프 기능 구현

---
+++ itta-bab-frontend/src/router/admin.js
@@ -0,0 +1,8 @@
+import bootcampForm from "@/components/admin/BootcampForm.vue";
+
+export default [
+    {
+        path: '/bootcamp/register',
+        component: bootcampForm,
+    }
+];
--- itta-bab-frontend/src/router/index.js
+++ itta-bab-frontend/src/router/index.js
@@ -1,28 +1,49 @@
 import {createRouter, createWebHistory} from "vue-router";
 
-import userRoutes from './user';
+import userRoutes from './user.js';
 import boardRoutes from './board.js';
 import groupRoutes from './group.js';
 import scheduleRoutes from './schedule.js';
 import storeRoutes from './store.js';
+import bootcampRoutes from './admin.js';
+import inquiryRoutes from './inquiry.js';
+import reportRoutes from "@/router/report.js";
 
 import HomeView from "@/views/Home.vue";
+import {useAuthStore} from "@/stores/auth.js";
 
 const routes = [
     {
         path: '/',
-        component: HomeView
+        component: HomeView,
+        meta: { requiresAuth: true }
     },
     ...userRoutes,
     ...boardRoutes,
     ...groupRoutes,
     ...scheduleRoutes,
     ...storeRoutes,
+    ...inquiryRoutes,
+    ...reportRoutes,
+    ...bootcampRoutes
 ];
 
 const router = createRouter({
    history: createWebHistory(),
    routes
+});
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+
+    if (to.meta.requiresAuth && !authStore.accessToken) {
+        next({ path: '/user/login' });
+    }
+    else if (authStore.accessToken && (to.path === '/user/login')) {
+        next({ path: '/' });
+    } else {
+        next(); // 나머지 경우는 계속 진행
+    }
 });
 
 export default router;

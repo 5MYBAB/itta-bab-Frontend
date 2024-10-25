a886c423d2d275a4dd20c69d480ba0cbb6a59c71
seolbin01 <se01bin@suwon.ac.kr>
2024-10-25 12:21:10 +0900

    feat: 이메일 인증 로직 추가

--- itta-bab-frontend/src/views/user/SignUp.vue
+++ itta-bab-frontend/src/views/user/SignUp.vue
@@ -3,21 +3,28 @@ import '@/assets/css/resetcss.css';
 import axios from "axios";
 import {useRouter} from "vue-router";
 import {computed, ref} from "vue";
+import './styles/SignUp.css'
 
 const loginId = ref('');
 const password = ref('');
 const confirmPassword = ref('');
+const username = ref('');
+const birthDate = ref('');
+const emailUser = ref('');
+const emailDomain = ref('');
+const fullEmail = ref('');
+const authCode = ref('');
 const router = useRouter();
 
 const handleSignUp = async () => {
   try {
     const response = await axios.post('http://localhost:8003/user/signup', {
-      username: "string",
+      username: username.value,
       loginId: loginId.value,
       pwd: password.value,
-      email: "string",
+      email: fullEmail.value,
       phone: "string",
-      birth: "2024-10-24",
+      birth: birthDate.value,
       courseId: 0,
       authCode: "string"
     });
@@ -34,15 +41,17 @@ const handleSignUp = async () => {
   }
 };
 
-const handleDuplicateCheckClick = async (loginId) => {
+const handleDuplicateCheckClick = async () => {
   try {
     const response = await axios.get('http://localhost:8003/user/id', {
       params: {
-        id: loginId
+        id: loginId.value
       }
     });
-    console.log(`아이디 중복확인 성공 ${response.status}`);
-    alert('사용할 수 있는 아이디입니다.');
+    if (response.status === 200) {
+      console.log(`아이디 중복확인 성공 ${response.status}`);
+      alert('사용할 수 있는 아이디입니다.');
+    }
   } catch (error) {
     if (error.response) {
       if (error.response.status === 409) {
@@ -53,6 +62,56 @@ const handleDuplicateCheckClick = async (loginId) => {
       }
     } else {
       console.error('아이디 중복확인 실패', error.message);
+    }
+  }
+};
+
+const emailSendMessage = ref('');
+
+const handleSendAuthCodeClick = async () => {
+  try {
+    fullEmail.value = `${emailUser.value}@${emailDomain.value}`;
+    emailSendMessage.value = '인증코드를 발송중입니다. 잠시 기다려주세요.';
+    const response = await axios.post('http://localhost:8003/user/email-request', {
+      email: fullEmail.value
+    });
+
+    if (response.status === 200) {
+      emailSendMessage.value = '이메일 인증코드를 전송하였습니다. 메일을 확인해주세요.';
+    }
+
+  } catch (error) {
+    console.error('인증번호 발송 실패', error);
+    emailSendMessage.value = '인증번호 발송에 실패했습니다.';
+  }
+};
+
+const emailAuthMessage = ref('');
+const isAuth = ref(false);
+
+const handleAuthCodeCheckClick = async () => {
+  try {
+    const response = await axios.post('http://localhost:8003/user/email-check', {
+      email: fullEmail.value,
+      authCode: authCode.value
+    });
+
+    if (response.status === 200) {
+      console.log(`이메일 인증 확인 성공 ${response.status}`);
+      emailAuthMessage.value = '인증이 확인되었습니다.';
+      isAuth.value = true;
+    }
+
+  } catch (error) {
+    if (error.response) {
+      if (error.response.status === 409) {
+        console.log('이메일 인증 확인 실패: 인증번호가 틀립니다.');
+        emailAuthMessage.value = '인증번호가 틀립니다. 다시 시도해주세요.';
+      } else {
+        console.error('이메일 인증 확인 실패:', error.response.data);
+      }
+    } else {
+      console.error('이메일 인증 확인 실패', error.message);
     }
   }
 };
@@ -98,7 +157,7 @@ const isValidPassword = computed(() => {
             <input type="text" v-model="loginId" required>
           </div>
           <div>
-            <input @click="handleDuplicateCheckClick(loginId)" type="button" value="중복확인">
+            <input @click="handleDuplicateCheckClick" type="button" value="중복확인" :disabled="!loginId">
           </div>
         </div>
         <div class="flex-box">
@@ -123,31 +182,47 @@ const isValidPassword = computed(() => {
         </div>
         <div class="flex-box">
           <div class="title">이름</div>
-          <div class="input-box"><input type="text"></div>
+          <div class="input-box">
+            <input type="text" v-model="username" required>
+          </div>
         </div>
         <div class="flex-box">
           <div class="title">생년월일</div>
           <div class="input-box">
-            <input type="date" data-placeholder="날짜 선택">
+            <input type="date" v-model="birthDate" data-placeholder="날짜 선택" required>
           </div>
         </div>
         <div class="flex-box">
           <div class="title">이메일</div>
-          <div class="inline input-box">
-            <input type="text" id="email-user">
-            <div class="email-icon">@</div>
-            <select name="email" class="email">
-              <option value="naver">naver</option>
-              <option value="gmail">gmail</option>
-              <option value="daum">daum</option>
-            </select>
+          <div>
+            <div class="email-and-button">
+              <div class="input-box">
+                <div class="email-box">
+                  <input type="text" v-model="emailUser" id="email-user" required>
+                  <div class="email-icon">@</div>
+                  <select v-model="emailDomain" class="email">
+                    <option value="naver.com" selected>naver.com</option>
+                    <option value="gmail.com">gmail.com</option>
+                  </select>
+                </div>
+              </div>
+              <div>
+                <input @click="handleSendAuthCodeClick" type="button" value="인증번호 발송" :disabled="!emailUser">
+              </div>
+            </div>
+            <div class="email-message">{{ emailSendMessage }}</div>
           </div>
-          <div><input type="button" value="인증번호 발송"></div>
         </div>
         <div class="flex-box">
           <div class="title">이메일 인증</div>
-          <div class="input-box"><input type="text"></div>
-          <div><input type="button" value="인증 확인"></div>
+          <div class="input-box">
+            <input type="text" v-model="authCode">
+            <div v-if="!isAuth" class="email-message">{{ emailAuthMessage }}</div>
+            <div v-if="isAuth" class="success-message">{{ emailAuthMessage }}</div>
+          </div>
+          <div>
+            <input @click="handleAuthCodeCheckClick" type="button" value="인증 확인" :disabled="fullEmail">
+          </div>
         </div>
         <div class="flex-box">
           <div class="title">휴대 전화</div>
@@ -211,224 +286,3 @@ const isValidPassword = computed(() => {
 
   </div>
 </template>
-
-<style scoped>
-.submit {
-  margin-top: 30px;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-}
-
-#submit-button {
-  width: 360px;
-  height: 44px;
-  background-color: var(--basic-yellow);
-  border-radius: 52px;
-}
-
-#agree {
-  margin: 10px;
-}
-
-.agree {
-  text-align: right;
-  display: flex;
-  justify-content: flex-end;
-}
-
-.block-border-box {
-  border-radius: 10px;
-  padding: 20px 24px;
-  border: var(--text-color) solid 2px;
-  color: var(--gray-font);
-
-}
-
-.phone-icon {
-  margin: 16px 3px;
-}
-
-.sub-notice {
-  margin-top: 20px;
-  color: var(--gray-font);
-  font-size: 14px;
-  margin-bottom: 32px;
-}
-
-.border {
-  font-size: 17px;
-  font-weight: 600;
-
-}
-
-.notice-wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-top: 30px;
-  padding-bottom: 10px;
-  border-bottom: var(--gray-font) solid 2px;
-}
-
-.bootcamp {
-  width: 136px;
-  border-radius: 10px;
-  background-color: var(--gray-input);
-  color: var(--gray-font);
-  border: none;
-  height: 44px;
-  padding-left: 12px;
-  margin-right: 22px;
-}
-
-.course {
-  width: 173px;
-  border-radius: 10px;
-  background-color: var(--gray-input);
-  color: var(--gray-font);
-  padding-left: 12px;
-  border: none;
-  height: 44px;
-}
-
-.back {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background-color: var(--background-color);
-}
-
-#phone1, #phone2, #phone3 {
-  width: 97px;
-  border-radius: 10px;
-  background-color: var(--gray-input);
-  display: flex;
-  text-align: center;
-}
-
-#email-user {
-  width: 184px;
-}
-
-.email {
-  width: 119px;
-  border-radius: 10px;
-  background-color: var(--gray-input);
-  border: none;
-  padding-left: 20px;
-  color: var(--gray-font);
-}
-
-.logo {
-  margin-top: 130px;
-  margin-bottom: 50px;
-}
-
-.white-box {
-  display: flex; /* Flexbox 사용 */
-  flex-direction: column; /* 수직 정렬 */
-  justify-content: center; /* 수평 가운데 정렬 */
-  align-items: center; /* 수직 가운데 정렬 */
-  box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.13) inset;
-  border-radius: 43px;
-  background-color: var(--white);
-  max-width: 800px; /* 원하는 최대 너비 설정 */
-  width: 100%; /* 너비를 100%로 설정하여 최대 너비를 활용 */
-  padding: 65px 150px;
-  margin: 0 auto; /* 수평 가운데 정렬 */
-}
-
-.top-title {
-  font-size: 25px;
-  text-align: center;
-  font-weight: 600;
-  margin-bottom: 50px;
-}
-
-.email-icon {
-  display: flex;
-  align-items: center;
-  margin: 18px 4px;
-}
-
-.flex-box {
-  display: flex;
-  align-items: center;
-  width: 635px;
-  margin: 18px 4px;
-}
-
-.title {
-  font-size: 17px;
-  font-weight: 600;
-  white-space: nowrap;
-  height: 44px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  width: 150px;
-}
-
-.small {
-  font-size: 14px;
-  font-weight: 500;
-  color: var(--gray-font);
-  padding-left: 20px;
-  margin-top: 5px;
-}
-
-input[type="button"] {
-  background-color: var(--basic-yellow);
-  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  color: var(--text-color);
-  text-align: center;
-  font-weight: 600;
-  border-radius: 10px;
-  width: 138px;
-  height: 44px;
-  border: none;
-}
-
-input[type="text"], [type="password"] {
-  border-radius: 10px;
-  background-color: var(--gray-input);
-  border: none;
-  width: 328px;
-  height: 44px;
-  padding-left: 20px;
-  color: var(--gray-font);
-  display: flex;
-  align-items: center;
-}
-
-input[type="date"], [type="number"] {
-  border-radius: 10px;
-  background-color: var(--gray-input);
-  border: none;
-  width: 328px;
-  height: 44px;
-  color: var(--gray-font);
-  display: flex;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-}
-
-.inline {
-  display: flex;
-}
-
-.input-box {
-  width: 348px;
-}
-
-.success-message, .valid{
-  color: var(--active-green);
-}
-
-.invalid {
-  color: var(--real-red);
-}
-</style>

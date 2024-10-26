63289cb8dc78f6d7321a2db1f20d9b2fc3d52be8
seolbin01 <se01bin@suwon.ac.kr>
2024-10-26 10:57:47 +0900

    fix: 회원정보 수정 로직 수정

--- itta-bab-frontend/src/views/user/UpdateUserInfo.vue
+++ itta-bab-frontend/src/views/user/UpdateUserInfo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import {computed, ref} from 'vue';
 import '@/assets/css/resetcss.css';
 
 const password = ref('');
@@ -15,6 +15,31 @@ const checkPasswords = () => {
     alert('회원 정보가 수정되었습니다.');
   }
 };
+
+const passwordCheckMessage = ref('');
+
+// 비밀번호 유효성 검사
+const isValidPassword = computed(() => {
+  if (password.value.length >= 8 && password.value.length <= 15 && password.value.match(/[A-Za-z]/) && password.value.match(/\d/)) {
+    passwordCheckMessage.value = '사용할 수 있는 비밀번호입니다.';
+    return true; // 유효한 비밀번호
+  } else if (password.value.length === 0) {
+    passwordCheckMessage.value = '영문, 숫자 포함하여 8~15자 입력해주세요 '; // 비밀번호가 비어있을 경우
+    return false; // 비밀번호가 유효하지 않음
+  } else {
+    passwordCheckMessage.value = '영문, 숫자 포함하여 8~15자 입력해주세요 '; // 유효하지 않을 경우
+    return false; // 비밀번호가 유효하지 않음
+  }
+});
+
+const passwordMismatch = computed(() => {
+  return password.value !== confirmPassword.value && confirmPassword.value.length > 0;
+});
+
+// 비밀번호 일치 여부
+const passwordMatch = computed(() => {
+  return password.value === confirmPassword.value && confirmPassword.value.length > 0;
+});
 </script>
 
 <template>
@@ -25,14 +50,23 @@ const checkPasswords = () => {
       <div class="form">
         <div class="inline">
           <div class="label">비밀번호</div>
-          <div>
-            <input type="password" v-model="password">
-            <div class="notice">영문, 숫자 포함하여 8~15자 입력해주세요</div>
+          <div class="input-box">
+            <input type="password" v-model="password" required @input="validatePassword">
+            <div
+                class="small"
+                :class="{'valid': isValidPassword, 'invalid': !isValidPassword && password.length > 0}"
+            >
+              {{ passwordCheckMessage }}
+            </div>
           </div>
         </div>
         <div class="inline">
           <div class="label">비밀번호 재확인</div>
-          <div><input type="password" v-model="confirmPassword"></div>
+          <div class="input-box">
+            <input type="password" v-model="confirmPassword" :disabled="!password" required>
+            <div v-if="passwordMismatch" class="error-message">비밀번호가 일치하지 않습니다.</div>
+            <div v-if="passwordMatch" class="success-message">비밀번호가 일치합니다.</div>
+          </div>
         </div>
       </div>
       <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
@@ -107,5 +141,29 @@ input[type="button"]{
   color: var(--real-red);
   font-size: 16px;
   margin-top: 10px;
+}
+
+.small{
+  font-size: 14px;
+  font-weight: 500;
+  color: var(--gray-font);
+  padding-left: 20px;
+  margin-top: 5px;
+}
+
+.success-message, .valid{
+  color: var(--active-green);
+  font-size: 14px;
+  font-weight: 500;
+  padding-left: 20px;
+  margin-top: 5px;
+}
+
+.error-message, .invalid {
+  color: var(--real-red);
+  font-size: 14px;
+  font-weight: 500;
+  padding-left: 20px;
+  margin-top: 5px;
 }
 </style>

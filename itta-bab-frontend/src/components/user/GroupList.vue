0f3fe4a5cfcc65d4a93b7fab5795f3729a9b7310
Pangtaek <zombie3632@gmail.com>
2024-10-26 16:32:12 +0900

    fix: 참여중인 모임 리스트 호출 오류 수정

--- itta-bab-frontend/src/components/user/GroupList.vue
+++ itta-bab-frontend/src/components/user/GroupList.vue
@@ -21,6 +21,7 @@ const fetchData = async () => {
 
     if (response.status === 200) {
       groupData.value = response.data;
+      console.log("참여중인 모임 정보");
       console.log(groupData.value);
     } else {
       console.log("참여중인 모임 정보를 불러오는데 실패했습니다.");
@@ -29,6 +30,17 @@ const fetchData = async () => {
     console.error('참여 중인 모임 정보 가져오기 실패', error);
   }
 };
+
+// 날짜 형식화 함수
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
 
 // 컴포넌트가 마운트될 때 데이터 호출
 onMounted(() => {
@@ -53,11 +65,11 @@ onMounted(() => {
       </div>
 
       <div v-for="group in groupData" :key="group.groupCategoryId" class="line">
-        <div class="title">{{ group.title }}</div>
-        <div class="end-time">{{ group.endTime }}</div>
+        <div class="title">{{ group.groupTitle }}</div>
+        <div class="end-time">{{ formatDate(group.endDate) }}</div>
         <div class="faUser">
           <font-awesome-icon :icon="['far', 'user']"/>
-          {{ group.currentParticipants }}/{{ group.maxParticipants }}
+          {{ group.userCounting }}
         </div>
       </div>
     </div>

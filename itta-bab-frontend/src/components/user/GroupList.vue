93456c40e8c27c494aac3d7577be88dcdef5ca8f
Pangtaek <zombie3632@gmail.com>
2024-10-26 16:23:50 +0900

    feat: 메인 페이지 참여중인 모임 API 연동

--- itta-bab-frontend/src/components/user/GroupList.vue
+++ itta-bab-frontend/src/components/user/GroupList.vue
@@ -1,37 +1,71 @@
 <script setup>
+import {useAuthStore} from "@/stores/auth.js";
+import {onMounted, ref} from "vue";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import axios from "axios";
+
+// 로그인 정보
+const authStore = useAuthStore();
+
+// 참여 중인 모임
+const groupData = ref([]);
+
+// REST API 호출 함수
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:8003/group/attend', {
+      headers: {
+        Authorization: `Bearer ${authStore.accessToken}`
+      }
+    });
+
+    if (response.status === 200) {
+      groupData.value = response.data;
+      console.log(groupData.value);
+    } else {
+      console.log("참여중인 모임 정보를 불러오는데 실패했습니다.");
+    }
+  } catch (error) {
+    console.error('참여 중인 모임 정보 가져오기 실패', error);
+  }
+};
+
+// 컴포넌트가 마운트될 때 데이터 호출
+onMounted(() => {
+  fetchData();
+});
 </script>
 
 <template>
   <div class="container">
     <div class="yellow">
       <div class="text-container">
-      모임 참여<div class="faArrow"><font-awesome-icon :icon="['fas', 'circle-arrow-right']" /></div>
+        모임 참여
+        <div class="faArrow">
+          <font-awesome-icon :icon="['fas', 'circle-arrow-right']"/>
+        </div>
       </div>
     </div>
+
     <div class="list">
-      <div class="line">
-        <div class="title">12시까지 밥모임</div>
-        <div class="end-time">12:00</div>
-        <div class="faUser"><font-awesome-icon :icon="['far', 'user']" /> 4/6</div>
+      <div v-if="groupData.length === 0" class="line">
+        <div class="title">참여 중인 모임이 없습니다.</div>
       </div>
-      <div class="line">
-        <div class="title">12시까지 밥모임</div>
-        <div class="end-time">12:00</div>
-        <div class="faUser"><font-awesome-icon :icon="['far', 'user']" /> 4/6</div>
-      </div>
-      <div class="line">
-        <div class="title">12시까지 밥모임</div>
-        <div class="end-time">12:00</div>
-        <div class="faUser"><font-awesome-icon :icon="['far', 'user']" /> 4/6</div>
+
+      <div v-for="group in groupData" :key="group.groupCategoryId" class="line">
+        <div class="title">{{ group.title }}</div>
+        <div class="end-time">{{ group.endTime }}</div>
+        <div class="faUser">
+          <font-awesome-icon :icon="['far', 'user']"/>
+          {{ group.currentParticipants }}/{{ group.maxParticipants }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
-.yellow{
+.yellow {
   border-radius: 20px 20px 0px 0px;
   background-color: var(--basic-yellow);
   height: 72px;
@@ -44,14 +78,17 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
   font-weight: 600;
   width: 414px;
 }
+
 .text-container {
   display: flex;
   align-items: center;
 }
+
 .faArrow {
-  margin : 10px;
+  margin: 10px;
 }
-.list{
+
+.list {
   background-color: var(--white);
   border-radius: 0px 0px 20px 20px;
   height: 513px;
@@ -59,7 +96,8 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
   display: flex;
   flex-direction: column;
 }
-.line{
+
+.line {
   margin: 10px 42px;
   border-bottom: 2px var(--gray-font) solid;
   padding-bottom: 4px;
@@ -67,11 +105,13 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
   justify-content: space-between;
   align-items: center;
 }
-.title{
+
+.title {
   font-size: 12px;
   font-weight: 400;
 }
-.end-time{
+
+.end-time {
   margin-left: 100px;
 }
 </style>
